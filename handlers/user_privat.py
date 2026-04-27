80e8386abef15ac8c8727159826d252a3cf1bce3
gost123098 <gosaarockij@gmail.com>
2026-04-27 20:17:05 +0300

    4 тема структура и команда бота

--- handlers/user_privat.py
+++ handlers/user_privat.py
@@ -1,4 +1,3 @@
-
 from aiogram.filters import CommandStart, Command
 from aiogram import types, Router
 
@@ -6,15 +5,20 @@ user_router = Router()
 
 @user_router.message(CommandStart())
 async def start_cmd(message: types.Message):
-    await message.answer("Привет\nЭто бот для продажи")
+    await message.answer("Привет\nЭто бот для продажи, чтобы узнать информацию о командах бота напишите '/info'")
 
-@user_router.message(Command("catalog"))
-async def catalog(message: types.Message):
-    await message.answer("Привет, это каталог!")
+@user_router.message(Command("shop"))
+async def shop(message: types.Message):
+    await message.answer("Валюта: 2 кристалла - 1 звезда")
 
-@user_router.message(Command("Lisa"))
-async def Lisa(message: types.Message):
-    await message.answer("Привет, булка! Я тебя люблю!!!")
+
+@user_router.message(Command("info"))
+async def info(message: types.Message):
+    await message.answer("Команды:")
+
+@user_router.message(Command("daily"))
+async def daily(message: types.Message):
+    await message.answer("Награды, которые даются каждый день:")
 
 @user_router.message()
 async def echo(message: types.Message):
